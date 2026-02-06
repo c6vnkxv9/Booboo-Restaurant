7ac6db9c66f28ba5124107264e392629bb595c6c
e8po88hX <106948222+c6vnkxv9@users.noreply.github.com>
2026-02-06 11:42:33 +0800

    feat:[frontstage]Index-Page (#9)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import IndexPage from './pages/index';
@@ -32,31 +31,6 @@ function App() {
 					<Route path="products" element={<AdminProducts />} />
 					<Route path="articles" element={<Articles />} />
 				</Route>
-=======
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Login from './pages/Login'
-import Products from './pages/Products'
-import Articles from './pages/Articles'
-import ProtectedRoute from './components/ProtectedRoute'
-import AdminLayout from './components/AdminLayout'
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route
-          element={
-            <ProtectedRoute>
-              <AdminLayout />
-            </ProtectedRoute>
-          }
-        >
-          <Route path="/products" element={<Products />} />
-          <Route path="/articles" element={<Articles />} />
-        </Route>
->>>>>>> 86529634bb5e9b54ae57abdef0415c425b481266
-
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
